6d4b94fe58d0765e4d38481ddd0ae9ef58bdf501
renjianshu <694742588@qq.com>
2020-03-30 22:03:35 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,29 @@
+/*
+ * @Author: renjianshu
+ * @Date: 2020-03-29 19:46:59
+ * @LastEditTime: 2020-03-30 21:33:59
+ * @LastEditors: renjianshu
+ * @Description:
+ * @FilePath: \mock\src\index.js
+ */
+
+const { ApolloServer,gql } = require("apollo-server")
+const { resolvers}  = require("./scalars")
+const path = require('path')
+const fs = require('fs')
+
+const typeStr = fs.readFileSync(path.join(__dirname,'schema.graphql'))
+const typeDefs = gql`${typeStr}`
+
+// The ApolloServer constructor requires two parameters: your schema
+// definition and your set of resolvers.
+const server = new ApolloServer({
+	typeDefs,
+	resolvers,
+	mocks:true
+});
+
+// The `listen` method launches a web server.
+server.listen().then(({  }) => {
+	console.log(`🚀  Server ready at localhost:4000`);
+});
